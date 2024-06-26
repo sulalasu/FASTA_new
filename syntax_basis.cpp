60de57313fb4aa22ccef349f2eced5bf6e63cca7
sulalasu <schaltjahr@hotmail.com>
2024-06-26 22:15:10 +0200

    commit to work remotely_

--- syntax_basis.cpp
+++ syntax_basis.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
 
+#define msgLOG(message, x) std::cout << message << ":  " << x << std::endl;
+#define LOG(x) std::cout << x << std::endl;
+
+class Studierender
+{
+public:
+    std::string m_vorname;
+    std::string m_nachname;
+    long m_matrikelnummer;
+
+    //constructor:
+    Studierender() {}
+    Studierender(const std::string i_nachname) 
+        : m_nachname(i_nachname) {}
+
+    //function:
+    void printNachname() {
+        msgLOG("name: ", m_nachname)
+    }
+};
+
+
 class Entity
 {
 private:
@@ -37,6 +59,78 @@ Entity* e;
     delete entity_3; 
     //delete e;
 }
+//___________________
+// Pointer allgemein: 
+//___________________
+
+int x = 10;
+int* pX;
+pX = &x; // & ist hier kein typ sondern ein operator. return(address of)
+//in einem schritt:
+// int y = 11;
+// int* pY = &y;
+msgLOG("original x: ", x);
+msgLOG("pointer x:  ", pX);
+
+
+//dereference (get value of pointer/address)
+msgLOG("dereference: ", *pX);
+
+Studierender* pSusi = new Studierender;
+//neuer block am heap wird reserviert. new gibt einen pointer zurück.
+// der ist in pSusi gespeichert.
+
+Studierender* pMaxi = new Studierender("mustermann");
+//geht:
+(*pMaxi).printNachname();
+pMaxi->printNachname();
+//geht nicht:
+//pMaxi.printNachname(); // pointer muss erst dereferenced werden!
+
+
+// im gegensatz zu referenzen können pointer auch null sein:
+Studierender* pNuller = NULL;
+Studierender* pNullerpointer = nullptr;
+
+// delete löscht das, wohin der pointer zeigt!
+msgLOG("delete pX", *pX);
+//delete pX; // Segmentation fault: Core dumped WEIL: delete nur in kombi mit 'new' beim instanzieren.
+delete pSusi;
+delete pMaxi;
+
+
+int* pNewX = new int;
+pNewX = &x;
+msgLOG("*pnewx: ", *pNewX);
+//delete pNewX; // only deletes pointer, not value stored at pointer!
+LOG("deleted pnewx ");
+msgLOG("x: ", x);
+msgLOG("*pNewX", *pNewX); //now points to random variable
+
+
+
+// setting a variable at pointer address:
+int* newP = new int;
+*newP = 17;
+msgLOG("*newP ", *newP);
+LOG("hello");
+msgLOG("newP: ", newP);
+
+int* z = new int(9);
+msgLOG("z address: ", z); 
+msgLOG("z: ", (*z));
+msgLOG("z: ", *z);
+*z = 8;
+msgLOG("z", *z);
+delete z;
+msgLOG("z after delte: ", *z) // random value for z! => UNDEFINED BEHAVIOUR!
+msgLOG("z address: ", z); 
+
+
+
+
+LOG("ende des programms");
+
 
 
 return 0;
